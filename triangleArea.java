2553f82643f48750ace59d02e98cfbf8bab2af72
demitech1024 <demitech1024@gmail.com>
2019-10-02 14:21:15 -0400

    Should be the final commit.

--- triangleArea.java
+++ triangleArea.java
@@ -4,5 +4,14 @@ public class triangleArea {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         
+        
+        System.out.print("\nEnter a height for the triangle : ");
+        double height = s.nextDouble();
+        System.out.print("\nEnter a base for the triangle   : ");
+        double base   = s.nextDouble();
+
+        double area = (height * base) / 2;
+        System.out.printf("\nThe area of a triangle with a height and base of %,.1f and %,.1f is %,.1f.\n\n", height, base, area);
+
     }
 }
